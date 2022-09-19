790736142b64e8cb1815e5ddd261f09fc53a344f
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-19 16:57:17 +0200

    ProductCard component styling added

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "../button/Button";
+import "./product-card.styles.scss";
 
 function ProductCard({ product }) {
   const { name, price, imageUrl } = product;
