9a6cad3c5eb47893bcbb43469b00b619e2ae7454
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-19 17:57:42 +0200

    useContext added to CartDropdown component

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartDropdownContext } from "../../context/CartDropdownContext";
 import Button from "../button/Button";
 import "./cart-dropdown.styles.scss";
 
 function CartDropdown() {
+  const { isOpen } = useContext(CartDropdownContext);
+
+  if (!isOpen) {
+    return;
+  }
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">item patata</div>
