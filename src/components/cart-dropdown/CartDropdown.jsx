b96b1c6aa3a2ad80c5ab3f55b2d6c89852378270
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-19 17:43:37 +0200

    CartDropdown stying added, scaffolding done

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./cart-dropdown.styles.scss";
 
 function CartDropdown() {
-  return <div>CartDropdown</div>;
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">item patata</div>
+      <button>Go to checkout</button>
+    </div>
+  );
 }
 
 export default CartDropdown;
