6a2e748bc72c92998ce7f54636753b4ddfeaaf55
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-20 16:05:02 +0200

    CartItem styling added

--- src/components/cart-item/CartItem.jsx
+++ src/components/cart-item/CartItem.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import "./cart-item.styles.scss";
 
 function CartItem({ cartItem }) {
-  const { name, quantity } = cartItem;
+  const { name, quantity, price, imageUrl } = cartItem;
   return (
-    <div>
-      <h2>{name}</h2>
-      <span>{quantity}</span>
+    <div className="cart-item-container">
+      <img src={imageUrl} alt={name} />
+      <div className="item-details">
+        <h2 className="name">{name}</h2>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
+      </div>
     </div>
   );
 }
