7c0a38ba445f2dd8499be8506ec9da51bf5e7a3b
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-20 11:23:34 +0200

    CartContext debugged, adding new item functionality refactorized

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,18 +1,19 @@
 import React, { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 import Button from "../button/Button";
+import CartItem from "../cart-item/CartItem";
 import "./cart-dropdown.styles.scss";
 
 function CartDropdown() {
-  const { isCartOpen } = useContext(CartContext);
-
-  //   if (!isCartOpen) {
-  //     return;
-  //   }
+  const { cartItems } = useContext(CartContext);
 
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items">item patata</div>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
       <Button>Go to checkout</Button>
     </div>
   );
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -2,28 +2,21 @@ import { createContext, useContext, useState } from "react";
 import CartDropdown from "../components/cart-dropdown/CartDropdown";
 
 const addCartItem = (cartItems, productToAdd) => {
-  const itemInCart = cartItems.filter((item) => item.id === productToAdd.id);
-  const newCartItems = [...cartItems];
-  //check if the product is present in cartItems array
-  if (itemInCart) {
-    //if yes, increment the amount of that product
-    const newItemQuantity = itemInCart.quantity + 1;
-    //then return the new array
-    newCartItems.map((item) => {
-      if (item.id === itemInCart.id) {
-        item.quantity = newItemQuantity;
-      }
+  const itemIsInCart = cartItems.find((item) => item.id === productToAdd.id);
+
+  if (itemIsInCart) {
+    const newCartItems = cartItems.map((item) => {
+      item.id === itemInCart.id
+        ? { ...item, quantity: item.quantity + 1 }
+        : item;
     });
     return newCartItems;
   } else {
-    //if not, add the product to the array
     const newItem = {
       ...productToAdd,
       quantity: 1,
     };
-    newCartItems.push(productToAdd);
-    return newCartItems;
-    //then return the new array
+    return [...cartItems, newItem];
   }
 };
 
@@ -37,11 +30,12 @@ export const CartContext = createContext({
 export const CartDropdownProvider = ({ children }) => {
   const [isCartOpen, setisCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
-  const value = { isCartOpen, setisCartOpen };
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
+
+  const value = { isCartOpen, setisCartOpen, cartItems, addItemToCart };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
