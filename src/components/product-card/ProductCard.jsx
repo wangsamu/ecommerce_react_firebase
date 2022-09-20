f82ddefb940ac1d4e453f431aaf5af755b7b5052
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-20 11:27:12 +0200

    add product functionality added to ProductCard component

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Button from "../button/Button";
 import "./product-card.styles.scss";
+import { CartContext } from "../../context/CartContext";
 
 function ProductCard({ product }) {
   const { name, price, imageUrl } = product;
+
+  const addProductToCart = () => addItemToCart(product);
 
   return (
     <div className="product-card-container">
@@ -12,7 +15,9 @@ function ProductCard({ product }) {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to cart</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add to cart
+      </Button>
     </div>
   );
 }
