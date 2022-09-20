435304c97052f7f4cdfd6ea2d3a8c258b1100849
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-20 16:48:53 +0200

    Checkout component scaffolding done, sourcing data from CartContext done

--- src/routes/checkout/CheckOut.jsx
+++ src/routes/checkout/CheckOut.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
 
 function CheckOut() {
-  return <div>CheckOut</div>;
+  const { cartItems } = useContext(CartContext);
+  //   const { imageUrl, name, id, price, quantity } = cartItems;
+
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Product</th>
+          <th>Description</th>
+          <th>Quantity</th>
+          <th>Price</th>
+          <th>Remove</th>
+        </tr>
+        {cartItems.map((product) => (
+          <tr key={product.id}>
+            <td>
+              <img src={product.imageUrl} alt={product.name} />
+            </td>
+            <td>{product.name}</td>
+            <td>{product.quantity}</td>
+            <td>{product.price}</td>
+            <td>Remove</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
 }
 
 export default CheckOut;
