e7b15460a8f4145b1763b1b8e60973d58631591c
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-21 23:21:16 +0200

    CheckoutItem component JSX modified

--- src/components/CheckoutItem/CheckoutItem.jsx
+++ src/components/CheckoutItem/CheckoutItem.jsx
@@ -12,22 +12,20 @@ function CheckoutItem(props) {
   const { id, name, imageUrl, price, quantity } = product;
 
   return (
-    <div>
-      <tr key={id}>
-        <td>
-          <img src={imageUrl} alt={name} />
-        </td>
-        <td>{name}</td>
-        <td>
-          <button onClick={handleDecreaseQuantity}>- </button>
-          <p>{quantity}</p>
-          <button onClick={handleIncreaseQuantity}> +</button>
-        </td>
-        <td>{price}</td>
-        <td>
-          <button onClick={handleRemove}>Remove Item</button>
-        </td>
-      </tr>
+    <div key={id} className="checkout-item-container">
+      <div>
+        <img src={imageUrl} alt={name} />
+      </div>
+      <div>{name}</div>
+      <div>
+        <button onClick={handleDecreaseQuantity}>- </button>
+        <p>{quantity}</p>
+        <button onClick={handleIncreaseQuantity}> +</button>
+      </div>
+      <div>{price}</div>
+      <div>
+        <button onClick={handleRemove}>Remove Item</button>
+      </div>
     </div>
   );
 }
