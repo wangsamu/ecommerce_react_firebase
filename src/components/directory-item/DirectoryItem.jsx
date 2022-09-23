01700b04e859766504de77e8cb5a0fe81f7f6249
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-23 15:37:47 +0200

    navigation functionality added to Directory component

--- src/components/directory-item/DirectoryItem.jsx
+++ src/components/directory-item/DirectoryItem.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./directory-item.styles.scss";
 
 function DirectoryItem({ id, title, imageUrl }) {
+  const navigate = useNavigate();
+
+  const goToCategory = () => {
+    navigate(`/shop/${title.toLowerCase()}`);
+  };
+
   return (
     <div key={id} className="directory-item-container">
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
       />
-      <div className="body">
-        <h2>{title}</h2>
+      <div className="body" onClick={goToCategory}>
+        <h2>{title} </h2>
         <p>Shop Now</p>
       </div>
     </div>
