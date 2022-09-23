6fbb8aeb04d21479d5013e6f8e0e90e1a13012ea
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-23 17:37:30 +0200

    button styling redone with styled component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import "./button.styles.scss";
-
-//we have 3 different styles of buttons
-//default
-//inverted
-//google sign-in
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+} from "./button.styles";
 
 const BUTTON_TYPE_CLASSES = {
+  base: "base",
   google: "google-sign-in",
   inverted: "inverted",
+};
+
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => {
+  return {
+    [BUTTON_TYPE_CLASSES.base]: BaseButton,
+    [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+    [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+  }[buttonType];
 };
 
 function Button({ children, buttonType, ...otherProps }) {
