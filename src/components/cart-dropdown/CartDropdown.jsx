121b727b45273df8c21234ecc1339d302ba50d57
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-26 12:27:56 +0200

    empty cart message added

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -17,9 +17,10 @@ function CartDropdown() {
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        {cartItems.map((item) => (
+        {cartItems.length > 0 ?         {cartItems.map((item) => (
           <CartItem key={item.id} cartItem={item} />
         ))}
+      : <span>Your cart is empty</span>}
       </div>
       <Button onClick={goToCheckout}>Go to checkout</Button>
     </div>
