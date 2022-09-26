0c7c28b0d02132026fd9dda48dbc20a35f39aa2e
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-26 16:54:08 +0200

    CategoryPreview component styling redone using styled components

--- src/components/CategoryPreview/CategoryPreview.jsx
+++ src/components/CategoryPreview/CategoryPreview.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ProductCard from "../product-card/ProductCard";
-import "./category-preview.styles.scss";
+import "./category-preview.styles";
+import {
+  CategoryPreviewContainer,
+  Title,
+  Preview,
+} from "./category-preview.styles";
+// import "./category-preview.styles.scss";
 
 function CategoryPreview({ title, products }) {
   return (
-    <div className="category-preview-container">
+    <CategoryPreviewContainer>
       <h2>
-        <Link className="title" to={title}>
-          {title.toUpperCase()}
-        </Link>
+        <Title to={title}>{title.toUpperCase()}</Title>
       </h2>
-      <div className="preview">
+      <Preview>
         {products
           .filter((_, idx) => idx < 4)
           .map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </div>
-    </div>
+      </Preview>
+    </CategoryPreviewContainer>
   );
 }
 
