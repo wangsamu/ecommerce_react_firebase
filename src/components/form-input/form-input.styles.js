ef7972f05d76c7cd66f8dd6ec515ab37af601278
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-26 16:24:50 +0200

    form-input.styles refactored

--- src/components/form-input/form-input.styles.js
+++ src/components/form-input/form-input.styles.js
@@ -10,7 +10,7 @@ const shrinkLabelStyles = css`
 `;
 
 const InputLabel = styled.div`
-  color: $sub-color;
+  color: ${subColor};
   font-size: 16px;
   font-weight: normal;
   position: absolute;
@@ -25,27 +25,31 @@ const InputLabel = styled.div`
 const Group = styled.div`
   position: relative;
   margin: 45px 0;
+
+  input[type="password"] {
+    letter-spacing: 0.3em;
+  }
 `;
 
 const Input = styled.input`
   background: none;
   background-color: white;
-  color: $sub-color;
+  color: ${subColor};
   font-size: 18px;
   padding: 10px 10px 10px 5px;
   display: block;
   width: 100%;
   border: none;
   border-radius: 0;
-  border-bottom: 1px solid $sub-color;
+  border-bottom: 1px solid ${subColor};
   margin: 25px 0;
 
   &:focus {
     outline: none;
   }
 
-  &:focus ~ .form-input-label {
-    @include shrinkLabel();
+  &:focus ~ ${InputLabel} {
+    ${shrinkLabelStyles}
   }
 `;
 
@@ -59,9 +63,7 @@ export { Group, Input, InputLabel };
   .form-input {
   }
 
-  input[type='password'] {
-    letter-spacing: 0.3em;
-  }
+
 
   .form-input-label {
   }
