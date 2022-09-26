3947864f39d821165a138709e41af33de3984dbb
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-26 16:30:18 +0200

    FormInput component shrink rendering logic refactored

--- src/components/form-input/FormInput.jsx
+++ src/components/form-input/FormInput.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Group, Input, InputLabel } from "./form-input.styles";
 
 function FormInput({ label, inputOptions }) {
-  const shrink = inputOptions.value.length > 0 ? true : false;
+  // const shrink = ;
   return (
     <Group>
       <Input required {...inputOptions} />
-      {label && <InputLabel shrink={shrink}>{label}</InputLabel>}
+      {label && (
+        //if "inputOptions.value.length" is 0, it'll be evaluated as falsy
+        <InputLabel shrink={inputOptions.value.length}>{label}</InputLabel>
+      )}
     </Group>
   );
 }
