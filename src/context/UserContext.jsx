4b91af63860141e68a9be78ac89e8dddb4bda41e
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-27 22:58:28 +0200

    USER_ACTION_TYPE created

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -11,12 +11,17 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
 const userReducer = (state, action) => {
   const { type, payload } = action;
 
   switch (type) {
     case "SET_CURRENT_USER":
       return {
+        ...state,
         currentUser: payload,
       };
     case "increment":
