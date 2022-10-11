d5d868ddcaeec829d4afd76914b6beca7ad3dda0
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-10-11 12:36:50 +0200

    reselect package installed, memoization applied to categories selector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,10 +1,17 @@
-export const selectCategoriesMap = (state) => {
-  const categoriesMap = state.categories.categories.reduce(
-    (acc, { title, items }) => {
+import { createSelector } from "reselect";
+
+const selectCategoriesReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoriesReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.categories.reduce((acc, { title, items }) => {
       acc[title.toLowerCase()] = items;
       return acc;
-    },
-    {}
-  );
-  return categoriesMap;
-};
+    }, {})
+);
