6c69386728eacc4cf8f06566745bb2a6bf08c8f5
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-10-12 13:37:39 +0200

    cart selector added

---
+++ src/store/cart/cart.selector.js
@@ -0,0 +1,27 @@
+import { createSelector } from "reselect";
+
+const selectCartReducer = (state) => state.createSelector;
+
+const selectIsCartOpen = createSelector(
+  [selectCartReducer],
+  (cart) => cart.cartItems
+);
+
+const selectCartItems = createSelector(
+  [selectCartReducer],
+  (cart) => cart.cartItems
+);
+
+const slectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (total, cartItem) => total + cartItem.quantity * cartItem,
+    price,
+    0
+  )
+);
+
+const selectCartCount = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
+);
+
+export { selectIsCartOpen };
