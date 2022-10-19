3610860122195f08ac06778a47b412f298361124
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-10-19 17:35:03 +0200

    Stripe Payment handler function created

--- src/components/payment-form/PaymentForm.jsx
+++ src/components/payment-form/PaymentForm.jsx
@@ -1,13 +1,24 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/Button";
-import { PaymentFormContainer } from "./PaymentForm.styled";
+import { FormContainer, PaymentFormContainer } from "./PaymentForm.styled";
 
 const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) return;
+  };
+
   return (
     <PaymentFormContainer>
-      <CardElement />
-      <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay now </Button>
+      <FormContainer>
+        <h2>Credit Card Payment:</h2>
+        <CardElement />
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay now </Button>
+      </FormContainer>
     </PaymentFormContainer>
   );
 };
--- src/components/payment-form/PaymentForm.styled.jsx
+++ src/components/payment-form/PaymentForm.styled.jsx
@@ -1,9 +1,16 @@
 import styled from "styled-components";
 
-export const PaymentFormContainer = styled.div`
+const PaymentFormContainer = styled.div`
   height: 300px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
+
+const FormContainer = styled.div`
+  height: 100px;
+  min-width: 500px;
+`;
+
+export { PaymentFormContainer, FormContainer };
